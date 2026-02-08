e92091766eb96f265343e7b32ad8daf4bca075c2
dvargas1 <daniel_v93@hotmail.com>
2026-02-08 11:44:52 -0300

    implementação de terraform usando MGC

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "api_key" {
+  type        = string
+  sensitive   = true
+  description = "Magalu Cloud API Key"
+}
+
+variable "region" {
+  type        = string
+  default     = "br-se1"
+  description = "Magalu Cloud region (br-se1 = Southeast, br-ne1 = Northeast)"
+}
+
+variable "machine_type" {
+  type        = string
+  default     = "BV2-2-40"
+  description = "VM machine type (BV1-1-40 = 1vCPU/1GB, BV2-2-40 = 2vCPU/2GB)"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "SSH public key content for VM access"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "ID of the default VPC (get from MagaluCloud console: Network > VPCs)"
+}
